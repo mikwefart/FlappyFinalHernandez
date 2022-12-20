23f45e20ba7da0bb9e526c157483347dad386035
mikwefart <116204698+mikwefart@users.noreply.github.com>
2022-12-20 12:45:11 -0800

    wrote scripts to flappy bird

--- FlappyBirdFinalHernandez/Assets/Scripts/GameControl.cs
+++ FlappyBirdFinalHernandez/Assets/Scripts/GameControl.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
-    public TextAlignment scoreText;
-    public GameObject gameOvertext;
-
-    private int score = 0;
+    public GameObject gameOverText;
+    public TMP_Text ScoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+
+    private int score = 0;
     // Start is called before the first frame update
     void Awake()
     {
@@ -34,11 +35,11 @@ public class GameControl : MonoBehaviour
         if (gameOver)
             return;
         score++;
-        scoreText.text = "Score: " + score.ToString();
+        ScoreText.text = "Score: " + score.ToString();
     }
     public void BirdDied()
     {
-        gameOvertext.SetActive(true);
+        gameOverText.SetActive(true);
         gameOver = true;
     }
 }
